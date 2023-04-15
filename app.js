08ea7e78e9f1ef105f60aabdcb6bca4e647e996d
ripperd-2882 <ripperd2882@gmail.com>
2023-04-16 02:04:24 +0530

    added compose page

--- app.js
+++ app.js
@@ -25,6 +25,10 @@ app.get("/about",function (req,res) {
 
 app.get("/contact",function (req,res) {
   res.render("contact",{contactContent:contactContent})  
+}
+)
+app.get("/compose",function (req,res) {
+  res.render("compose")  
 })
 
 
