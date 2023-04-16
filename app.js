d04a8eea4b6a3b10fa69f77d8b30dd831e5a4a3e
ripperd-2882 <ripperd2882@gmail.com>
2023-04-17 01:04:44 +0530

    added compose.ejs features

--- app.js
+++ app.js
@@ -10,39 +10,54 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 
 const app = express();
 
+const posts = [];
+
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
-app.get("/",function (req,res) {
-  res.render("home",{homeStartingContent:homeStartingContent})  
+app.get("/", function (req, res) {
+  res.render("home", { homeStartingContent: homeStartingContent, posts: posts })
 })
 
-app.get("/about",function (req,res) {
-  res.render("about",{aboutContent:aboutContent})  
+app.get("/about", function (req, res) {
+  res.render("about", { aboutContent: aboutContent })
 })
 
-app.get("/contact",function (req,res) {
-  res.render("contact",{contactContent:contactContent})  
+app.get("/contact", function (req, res) {
+  res.render("contact", { contactContent: contactContent })
 }
 )
 
-app.get("/compose",function (req,res) {
-  res.render("compose")  
+app.get("/compose", function (req, res) {
+  res.render("compose")
 })
 
-app.post("/compose",function (req,res) {
-  
-  console.log(req.body.postTitle);
+app.post("/compose", function (req, res) {
+
+  const post = {
+    title: req.body.postTitle,
+    content: req.body.postBody
+  };
+  posts.push(post);
+  res.redirect("/");
+})
+
+app.get("/posts/:postName",function (req,res) {
+  const requestedTitle=req.params.postName;
+  posts.forEach(function (post) {
+    let storedTitle=post.title;
+    
+    if (storedTitle===requestedTitle)
+    console.log("Match Found!");
+  })
 })
 
 
 
 
 
-
-
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
