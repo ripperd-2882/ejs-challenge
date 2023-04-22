ea779f8e979233e043c3fde2974f1da04fedfcea
ripperd-2882 <ripperd2882@gmail.com>
2023-04-23 01:24:48 +0530

    Connected to mongoDB atlas

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ const ejs = require("ejs");
 const _ = require("lodash");
 const mongoose=require("mongoose");
 
-mongoose.connect("mongodb://127.0.0.1:27017/blogDB", {useNewUrlParser: true});
+// mongoose.connect("mongodb://127.0.0.1:27017/blogDB", {useNewUrlParser: true});
+mongoose.connect("mongodb+srv://ripperd2882:LaHjA0eWWI4xgYAl@cluster0.jmelfkg.mongodb.net/blogDB");
 
 const postSchema = {
   title: String,
