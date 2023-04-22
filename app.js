6bf457ffdc8b872949a03ac36be503ee2d997836
ripperd-2882 <ripperd2882@gmail.com>
2023-04-22 17:24:13 +0530

    Revert "Connected to mongoDB atlas for backend"
    This reverts commit d43f983d61e39ef26dc130a4cae287905a50774f.

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const ejs = require("ejs");
 const _ = require("lodash");
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb+srv://ripperd2882:LaHjA0eWWI4xgYAl@cluster0.jmelfkg.mongodb.net/blogDB");
+mongoose.connect("mongodb://127.0.0.1:27017/blogDB");
 
 const postSchema = {
   title: String,
