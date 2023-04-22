9c9f201203c596551b65c1b825b89ed2c2cf0841
ripperd-2882 <ripperd2882@gmail.com>
2023-04-22 15:01:13 +0530

    Added mongoDB database, mongoose,data shown on home page

--- app.js
+++ app.js
@@ -4,6 +4,16 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require("lodash");
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://127.0.0.1:27017/blogDB");
+
+const postSchema = {
+  title: String,
+  content: String
+};
+
+const Post = mongoose.model("Post", postSchema);
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -11,7 +21,8 @@ const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rho
 
 const app = express();
 
-const posts = [];
+//Used MongoDB
+// const posts = [];
 
 app.set('view engine', 'ejs');
 
@@ -19,8 +30,14 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-  res.render("home", { homeStartingContent: homeStartingContent, posts: posts })
+  Post.find({}).then(function (posts) {
+    res.render("home", { homeStartingContent: homeStartingContent, posts: posts })
+  })
 })
+
+// app.get("/", function (req, res) {
+//   res.render("home", { homeStartingContent: homeStartingContent, posts: posts })
+// })
 
 app.get("/about", function (req, res) {
   res.render("about", { aboutContent: aboutContent })
@@ -37,11 +54,17 @@ app.get("/compose", function (req, res) {
 
 app.post("/compose", function (req, res) {
 
-  const post = {
+  const post = new Post({
     title: req.body.postTitle,
     content: req.body.postBody
-  };
-  posts.push(post);
+  });
+  post.save();
+
+  // const post = {
+  //   title: req.body.postTitle,
+  //   content: req.body.postBody
+  // };
+  // posts.push(post);
   res.redirect("/");
 })
 
